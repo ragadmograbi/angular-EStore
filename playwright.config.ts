e58f599c555a011e6c63ad58021cb8f2f9c76fdd
ragadmograbi <ragadmograbi10@gmail.com>
2022-11-28 21:05:14 +0200

    clean-up

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,44 +1,15 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 const config: PlaywrightTestConfig = {
   testDir: './E2Etests',
-  /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 5 * 60 * 1000,
+  globalTimeout: 10 * 60 * 1000,
   expect: {
-    /**
-     * Maximum time expect() should wait for the condition to be met.
-     * For example in `await expect(locator).toHaveText();`
-     */
-    timeout: 15 * 60 * 1000
+    timeout: 1 * 60 * 1000
   },
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-
-  /* Retry on CI only */
-  retries: 0,
-  /* Opt out of parallel tests on CI. */
-  workers: 1,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  // reporter: 'html',
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
     baseURL: 'http://localhost:4200/',
     headless: false,
     launchOptions: {
@@ -50,9 +21,9 @@ const config: PlaywrightTestConfig = {
     port: 4200,
     command: "npm run start",
     timeout: 15 * 60 * 1000,
-    reuseExistingServer: true
+    reuseExistingServer: true,
   },
-  /* Configure projects for major browsers */
+
   projects: [
     {
       name: 'chromium',
